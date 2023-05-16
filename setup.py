88df0d7c0b54b735ec2d330d491e8240b714aae9
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2023-05-16 19:28:05 +0200

    - Add train & validation datasets (they're small so yolo) - Create a stub of data exploration notebook - Fix a minor styling issue in setup.py
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup
 setup(
     name="clickbait",
     version="0.0",
-    description="Final project for NLP 2023 course @ MIM UW: SemEval clickbait spoiling challenge",
+    description="Final project for NLP 2023 course @ MIM UW: "
+                "SemEval clickbait spoiling challenge",
     author="ASDF",
     author_email='asdf@asdf.com',
     packages=find_packages(),
