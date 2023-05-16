7bb497dcfa39c89d72efcb0d9a26b76bc6b630c4
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2023-05-16 18:43:28 +0200

    Setup basic directory structure
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="clickbait",
+    version="0.0",
+    description="Final project for NLP 2023 course @ MIM UW: SemEval clickbait spoiling challenge",
+    author="ASDF",
+    author_email='asdf@asdf.com',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "torch",
+    ],
+    setup_requires=['wheel']
+)
